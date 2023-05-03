9eeec827ae5fd4e82fc8f67bfcb0d2d3fefe662c
kyoottee <jimmyricks.grajo@ctu.edu.ph>
2023-05-04 05:12:27 +0800

    with pics!

--- myscripts.js
+++ myscripts.js
@@ -10,3 +10,14 @@ $("button").click(function (event) {
     }
   );
 });
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
