ec54a8c6508a1ee8856005965a61c1212d8787b1
kyoottee <jimmyricks.grajo@ctu.edu.ph>
2023-05-17 00:54:03 +0800

    HATDOG

--- myscripts.js
+++ myscripts.js
@@ -5,19 +5,6 @@ $("button").click(function (event) {
       width: "toggle",
     },
     150,
-    function () {
-      // animation complete
-    }
+    function () {}
   );
 });
-
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 1000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
