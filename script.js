ae4e6d316072d41c59da5962e17c5ac850430fa7
kyoottee <jimmyricks.grajo@ctu.edu.ph>
2023-05-17 02:01:42 +0800

    HAPIT NA

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+function changeBackground(imagePath) {
+  document.body.style.backgroundImage =
+    "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.85)), url('" +
+    imagePath +
+    "')";
+  document.body.style.transition = "all 2.5s";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundRepeat = "no-repeat";
+}
